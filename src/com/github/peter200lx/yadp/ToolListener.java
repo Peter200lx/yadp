4943044b4662c89edfa3a891c551554cc0494b79
Peter Olson <peter.olson@gmail.com>
2012-01-30 00:42:30 -0800

    Massive change 2, permissions now work, start of pretty print.
    Todo:
    Add in configuration file saving/loading.
    Expand data2Str data pretty print function.
    Add complex data type scrolling.

--- src/com/github/peter200lx/yadp/ToolListener.java
+++ src/com/github/peter200lx/yadp/ToolListener.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.TreeSpecies;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,66 +12,141 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.Crops;
+import org.bukkit.material.Ladder;
+import org.bukkit.material.Leaves;
+import org.bukkit.material.LongGrass;
+import org.bukkit.material.Step;
+import org.bukkit.material.TrapDoor;
+import org.bukkit.material.Wool;
 
 public class ToolListener implements Listener {
 	protected static final Logger log = Logger.getLogger("Minecraft");
 
 	@EventHandler
 	public void catchinteract(PlayerInteractEvent event){
-		if (event.getAction().equals(Action.LEFT_CLICK_AIR)) {
+		Player subject = event.getPlayer();
+		if ((event.getAction().equals(Action.LEFT_CLICK_AIR)) ||
+				event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
 			//TODO Investigate how to disable left-click destruction with creative mode
-			event.getPlayer().sendMessage("YADP tools do not work with left-click in creative mode");
+			if(YADP.debug) log.info("[catchinteract]"+subject.getName()+" "+
+					event.getAction().toString() + " on air (likely in creative mode)");
 		} else {
-			Player subject = event.getPlayer();
-			String call = YADP.tools.get(subject.getItemInHand().getType());
-			if(call != null) {
-				if((call == "dupeTool")&&(subject.hasPermission("yadp.tool.dupe")))
+			Material tool = subject.getItemInHand().getType();
+			if(YADP.tools.containsValue(tool)) {
+				if((YADP.tools.get("dupeTool")==tool)&&(subject.hasPermission("yadp.tool.dupe")))
 					this.dupeTool(event);
-				else if((call == "scrollTool")&&(subject.hasPermission("yadp.tool.scroll")))
+				else if((YADP.tools.get("scrollTool")==tool)&&(subject.hasPermission("yadp.tool.scroll")))
 					this.scrollTool(event);
 			}
 		}
 	}
-	
-	private void dupeTool(PlayerInteractEvent event){
-		Player subject = event.getPlayer();
-		Block clicked = event.getClickedBlock();
 
+	@SuppressWarnings("deprecation")	//TODO Investigate replacement .updateInventory()
+	private void dupeTool(PlayerInteractEvent event){
 		if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+			Player subject = event.getPlayer();
+
+			Block clicked = event.getClickedBlock();
+			if(YADP.debug) log.info("[dupeTool] "+subject.getName()+" clicked "+clicked.getType());
+
 			Material toUse = YADP.dupeMap.get(clicked.getType());
 			if(toUse == null)
 				toUse = clicked.getType();
 
-			if(clicked.getData() == 0) {
-				subject.getInventory().addItem(new ItemStack(toUse, 64));
-	        	subject.sendMessage(ChatColor.GREEN + "Enjoy your " + ChatColor.GOLD + 
-		        		  clicked.getType());
-	        	subject.updateInventory();
-			} else {
+			if((YADP.dataMap.containsKey(clicked.getType()))||(clicked.getData() != 0)) {
 				subject.getInventory().addItem(new ItemStack(toUse, 64, (short) 0, clicked.getData()));
-	        	subject.sendMessage(ChatColor.GREEN + "Enjoy your " + ChatColor.GOLD + 
-		        		  clicked.getType() + ChatColor.WHITE + ":" + 
-		        		  ChatColor.BLUE + clicked.getData());
-	        	subject.updateInventory();
+				subject.updateInventory();
+			} else {
+				subject.getInventory().addItem(new ItemStack(toUse, 64));
+				subject.updateInventory();
+			}
+			if(YADP.dataMap.containsKey(toUse))
+			{
+				subject.sendMessage(ChatColor.GREEN + "Enjoy your " + ChatColor.GOLD +
+						toUse.toString() + ChatColor.WHITE + ":" +
+						ChatColor.BLUE + data2Str(clicked));
+			} else {
+				subject.sendMessage(ChatColor.GREEN + "Enjoy your " + ChatColor.GOLD +
+							toUse.toString());
 			}
 		}
 	}
 	
 	private void scrollTool(PlayerInteractEvent event) {
-		if(YADP.dataMap.containsKey(event.getClickedBlock().getType())) {
-			int max = YADP.dataMap.get(event.getClickedBlock().getType());
-			byte data = event.getClickedBlock().getData();
+		Action act = event.getAction();
+		if(act.equals(Action.LEFT_CLICK_BLOCK)||(act.equals(Action.RIGHT_CLICK_BLOCK))) {
+			if(YADP.dataMap.containsKey(event.getClickedBlock().getType())) {
+				Block clicked = event.getClickedBlock();
+				if(YADP.debug) log.info("[scrollTool] "+event.getPlayer().getName()+" clicked "+clicked.getType());
+				int max = YADP.dataMap.get(clicked.getType());
+				byte data = clicked.getData();
 
-			if(event.getAction().equals(Action.LEFT_CLICK_BLOCK)){
-				data = (byte) ((data - 1) % max);
-				event.getPlayer().sendMessage("Data value scrolled, you just can't see it");
-			} else if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
-				data = (byte) ((data + 1) % max);
+				if(max != 0) {
+					if(act.equals(Action.LEFT_CLICK_BLOCK)){
+						data = (byte) ((data - 1) % max);
+						event.getPlayer().sendMessage("Data value scrolled, you just can't see it");
+					} else if(act.equals(Action.RIGHT_CLICK_BLOCK)){
+						data = (byte) ((data + 1) % max);
+					}
+				} else {
+					//TODO Add special case if statements here for complex scrolls
+					//if(clicked.getType()==Material.NOTE_BLOCK) { }
+					event.getPlayer().sendMessage(clicked.getType()+" is not yet scrollable");
+				}
+
+				clicked.setData(data, false);
+
+				event.getPlayer().sendMessage(ChatColor.GREEN + "Block is now " + ChatColor.GOLD +
+						clicked.getType() + ChatColor.WHITE + ":" +
+						ChatColor.BLUE + data2Str(clicked));
 			}
-			event.getClickedBlock().setData(data, false);
+		}
+	}
+
+	//TODO Fill out these print statements
+	//		Look at YAML.dupeMap for more types to investigate
+	private String data2Str(Block b) {
+		Material type = b.getType();
+		byte data = b.getData();
+		int max = YADP.dataMap.get(type);
+		if(YADP.debug) log.info("[data2str] Block "+type.toString()+":"+data);
+		if(Material.WOOL == type) {
+			Wool tmp = new Wool(type,data);
+			return tmp.getColor().toString();
+		} else if(Material.CROPS == type) {
+			Crops tmp = new Crops(type,data);
+			return tmp.getState().toString();
+		} else if(Material.LONG_GRASS == type) {
+			LongGrass tmp = new LongGrass(type,data);
+			return tmp.getSpecies().toString();
+		} else if(Material.SAPLING == type) {
+			return TreeSpecies.getByData(
+					(byte)(data%((max==0)?3:max))	).toString();
+		} else if(Material.LOG == type) {
+			return TreeSpecies.getByData(
+					(byte)(data%((max==0)?3:max))	).toString();
+		} else if(Material.LEAVES == type) {
+			Leaves tmp = new Leaves(type,data);
+			if(tmp.getSpecies() == null)
+				return ""+data; //TODO Find a proper way to cast a byte to a string
+			else
+				return tmp.getSpecies().toString();
+		} else if(Material.LADDER == type) {
+			Ladder tmp = new Ladder(type,data);
+			return tmp.getAttachedFace().toString();
+		} else if(Material.TRAP_DOOR == type) {
+			TrapDoor tmp = new TrapDoor(type,data);
+			return tmp.getAttachedFace().toString() + " is " +
+									(tmp.isOpen()?"Open":"Closed");
+		} else if(Material.STEP == type) {
+			Step tmp = new Step(type,data);
+			return tmp.getMaterial().toString();
+		} else if(Material.DOUBLE_STEP == type) {
+			Step tmp = new Step(type,data);
+			return tmp.getMaterial().toString();
 		} else {
-			event.getPlayer().sendMessage( event.getClickedBlock().getType().toString()+
-					" is not scrollable.");
+			return "" + data; //TODO Find a proper way to cast a byte to a string
 		}
 	}
 }
--- src/com/github/peter200lx/yadp/YADP.java
+++ src/com/github/peter200lx/yadp/YADP.java
@@ -3,7 +3,11 @@ package com.github.peter200lx.yadp;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
+import org.bukkit.CropState;
+import org.bukkit.DyeColor;
+import org.bukkit.GrassSpecies;
 import org.bukkit.Material;
+import org.bukkit.TreeSpecies;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,91 +17,102 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class YADP extends JavaPlugin {
 	private Logger log = Logger.getLogger("Minecraft");
 	
-	static HashMap<Material, String> tools = new HashMap<Material, String>();
-    public static HashMap<Material, Material> dupeMap = new HashMap<Material, Material>();
-    static {
-    	dupeMap.put(Material.BED_BLOCK, Material.BED);
-    	dupeMap.put(Material.PISTON_EXTENSION, Material.PISTON_BASE);
-    	dupeMap.put(Material.PISTON_MOVING_PIECE, Material.PISTON_BASE);
-    	dupeMap.put(Material.CROPS, Material.SEEDS);
-    	dupeMap.put(Material.REDSTONE_WIRE, Material.REDSTONE);
-    	dupeMap.put(Material.SIGN_POST, Material.SIGN);
-    	dupeMap.put(Material.WOODEN_DOOR, Material.WOOD_DOOR);
-    	dupeMap.put(Material.WALL_SIGN, Material.SIGN);
-    	dupeMap.put(Material.IRON_DOOR_BLOCK, Material.IRON_DOOR);
-    	dupeMap.put(Material.REDSTONE_TORCH_OFF, Material.REDSTONE_TORCH_ON);
-    	dupeMap.put(Material.SNOW, Material.SNOW_BALL); //TODO Is this a good conversion?
-    	dupeMap.put(Material.SUGAR_CANE_BLOCK, Material.SUGAR_CANE);
-    	dupeMap.put(Material.DIODE_BLOCK_OFF, Material.DIODE);
-    	dupeMap.put(Material.DIODE_BLOCK_ON, Material.DIODE);
-    	dupeMap.put(Material.LOCKED_CHEST, Material.CHEST);
-    	dupeMap.put(Material.PUMPKIN_STEM, Material.PUMPKIN_SEEDS);
-    	dupeMap.put(Material.MELON_STEM, Material.MELON_SEEDS);
-    	//TODO blocks below are a limit check, not for actual release
-    	dupeMap.put(Material.BEDROCK, Material.APPLE);
-    	dupeMap.put(Material.TNT, Material.DIRT);
-    }
-    public static HashMap<Material, Integer> dataMap = new HashMap<Material, Integer>();
-    static {
-    	dataMap.put(Material.LOG, 4);
-    	dataMap.put(Material.LEAVES, 4);
-    	//dataMap.put(Material.JUKEBOX, 12);		//TODO More research into music
-    	dataMap.put(Material.SAPLING, 3);			//TODO More research into age
-    	dataMap.put(Material.CACTUS, 16);
-    	dataMap.put(Material.SUGAR_CANE_BLOCK, 16);
-    	dataMap.put(Material.CROPS, 8);
-    	dataMap.put(Material.NETHER_WARTS, 4);
-    	dataMap.put(Material.PUMPKIN_STEM, 8);
-    	dataMap.put(Material.MELON_STEM, 8);
-    	dataMap.put(Material.WOOL, 16);
-    	dataMap.put(Material.TORCH, 6);
-    	dataMap.put(Material.REDSTONE_TORCH_OFF, 6);
-    	dataMap.put(Material.REDSTONE_TORCH_ON, 6);
-    	dataMap.put(Material.RAILS, 10);
-    	//dataMap.put(Material.POWERED_RAIL, 6);	//TODO More research into powered state
-    	//dataMap.put(Material.DETECTOR_RAIL, 6);	//TODO More research into powered state
-    	dataMap.put(Material.WOOD_STAIRS, 4);
-    	dataMap.put(Material.COBBLESTONE_STAIRS, 4);
-    	dataMap.put(Material.BRICK_STAIRS, 4);
-    	dataMap.put(Material.SMOOTH_STAIRS, 4);
-    	dataMap.put(Material.NETHER_BRICK_STAIRS, 4);
-    	//dataMap.put(Material.LEVER, 5);			//TODO More research into powered states
-    	//dataMap.put(Material.WOODEN_DOOR, 4);		//TODO More research into half-state
-    	//dataMap.put(Material.IRON_DOOR_BLOCK, 4);	//TODO More research into half-state
-    	dataMap.put(Material.STONE_BUTTON, 4);
-    	dataMap.put(Material.SIGN_POST, 16);
-    	//dataMap.put(Material.LADDER, 6);			//TODO More research into missing 0x0 & 0x1
-    	//dataMap.put(Material.WALL_SIGN, 6);		//TODO More research into missing 0x0 & 0x1
-    	//dataMap.put(Material.FURNACE, 6);			//TODO More research into missing 0x0 & 0x1
-    	//dataMap.put(Material.DISPENSER, 6);		//TODO More research into missing 0x0 & 0x1
-    	//dataMap.put(Material.CHEST, 6);			//TODO More research into missing 0x0 & 0x1
-    	dataMap.put(Material.PUMPKIN, 4);
-    	dataMap.put(Material.JACK_O_LANTERN, 4);
-    	//dataMap.put(Material.STONE_PLATE, 2);		//TODO More research into powered states
-    	//dataMap.put(Material.WOOD_PLATE, 2);		//TODO More research into powered states
-    	dataMap.put(Material.STEP, 7);
-    	dataMap.put(Material.DOUBLE_STEP, 7);
-    	dataMap.put(Material.SNOW, 8);
-    	dataMap.put(Material.CAKE_BLOCK, 6);
-    	//dataMap.put(Material.BED_BLOCK, 6);		//TODO More research into foot/head bit
-    	//dataMap.put(Material.DIODE_BLOCK_OFF, 8);	//TODO More research into delay/direction
-    	//dataMap.put(Material.DIODE_BLOCK_ON, 8);	//TODO More research into delay/direction
-    	//dataMap.put(Material.REDSTONE_WIRE, 16);	//TODO Likely not as it is power level
-    	dataMap.put(Material.LONG_GRASS, 3);
-    	dataMap.put(Material.TRAP_DOOR, 5);
-    	//dataMap.put(Material.PISTON_BASE, 6);		//TODO More research into bit 0x8
-    	//dataMap.put(Material.PISTON_STICKY_BASE, 6);	//TODO same as above
-    	//dataMap.put(Material.PISTON_EXTENSION, 6);	// Uhh, no
-    	dataMap.put(Material.SMOOTH_BRICK, 3);
-    	dataMap.put(Material.HUGE_MUSHROOM_1, 11);
-    	dataMap.put(Material.HUGE_MUSHROOM_2, 11);
-    	dataMap.put(Material.VINE, 16);				//TODO Look into breakdown
-    	dataMap.put(Material.FENCE_GATE, 4);		//TODO Look into keeping open state
-    	dataMap.put(Material.MONSTER_EGGS, 3);
-    	dataMap.put(Material.CAULDRON, 4);
-    	dataMap.put(Material.ENDER_PORTAL_FRAME, 4);	//TODO More research into "empty"
-    	//dataMap.put(Material.EGG, ??);			//TODO More research into spawning    	
-    }
+	public static boolean debug = false;
+
+	static HashMap<String, Material> tools = new HashMap<String, Material>();
+
+	public static HashMap<Material, Material> dupeMap = new HashMap<Material, Material>();
+	static {
+		dupeMap.put(Material.BED_BLOCK, Material.BED);
+		dupeMap.put(Material.PISTON_EXTENSION, Material.PISTON_BASE);
+		dupeMap.put(Material.PISTON_MOVING_PIECE, Material.PISTON_BASE);
+		dupeMap.put(Material.CROPS, Material.SEEDS);
+		dupeMap.put(Material.REDSTONE_WIRE, Material.REDSTONE);
+		dupeMap.put(Material.SIGN_POST, Material.SIGN);
+		dupeMap.put(Material.WOODEN_DOOR, Material.WOOD_DOOR);
+		dupeMap.put(Material.WALL_SIGN, Material.SIGN);
+		dupeMap.put(Material.IRON_DOOR_BLOCK, Material.IRON_DOOR);
+		dupeMap.put(Material.REDSTONE_TORCH_OFF, Material.REDSTONE_TORCH_ON);
+		dupeMap.put(Material.SNOW, Material.SNOW_BALL); //TODO Is this a good conversion?
+		dupeMap.put(Material.SUGAR_CANE_BLOCK, Material.SUGAR_CANE);
+		dupeMap.put(Material.DIODE_BLOCK_OFF, Material.DIODE);
+		dupeMap.put(Material.DIODE_BLOCK_ON, Material.DIODE);
+		dupeMap.put(Material.LOCKED_CHEST, Material.CHEST);
+		dupeMap.put(Material.PUMPKIN_STEM, Material.PUMPKIN_SEEDS);
+		dupeMap.put(Material.MELON_STEM, Material.MELON_SEEDS);
+		//TODO blocks below are a limit check, not for actual release
+		dupeMap.put(Material.BEDROCK, Material.APPLE);
+	}
+
+	public static HashMap<Material, Integer> dataMap = new HashMap<Material, Integer>();
+	static {
+		//If the integer is 0, that means that a simple numerical shift won't work
+		dataMap.put(Material.LOG, TreeSpecies.values().length);
+		dataMap.put(Material.LEAVES, 4);
+		dataMap.put(Material.JUKEBOX, 0);			//TODO More research into music
+		dataMap.put(Material.SAPLING, TreeSpecies.values().length);	//TODO More research into age
+		dataMap.put(Material.CACTUS, 16);
+		dataMap.put(Material.SUGAR_CANE_BLOCK, 16);
+		//TODO Add Water and Lava? Likely not	//TODO What if tool id is a bucket
+		dataMap.put(Material.SOIL, 8);				//TODO More research into if should be modified
+		dataMap.put(Material.CROPS, CropState.values().length);
+		dataMap.put(Material.NETHER_WARTS, 4);
+		dataMap.put(Material.PUMPKIN_STEM, 8);
+		dataMap.put(Material.MELON_STEM, 8);
+		dataMap.put(Material.WOOL, DyeColor.values().length);
+		//TODO Add Dyes? Likely not
+		dataMap.put(Material.TORCH, 6);
+		dataMap.put(Material.REDSTONE_TORCH_OFF, 6);
+		dataMap.put(Material.REDSTONE_TORCH_ON, 6);
+		dataMap.put(Material.RAILS, 10);
+		dataMap.put(Material.POWERED_RAIL, 0);		//TODO More research into powered state
+		dataMap.put(Material.DETECTOR_RAIL, 0);		//TODO More research into powered state
+		dataMap.put(Material.WOOD_STAIRS, 4);
+		dataMap.put(Material.COBBLESTONE_STAIRS, 4);
+		dataMap.put(Material.BRICK_STAIRS, 4);
+		dataMap.put(Material.SMOOTH_STAIRS, 4);
+		dataMap.put(Material.NETHER_BRICK_STAIRS, 4);
+		dataMap.put(Material.LEVER, 0);				//TODO More research into powered states
+		dataMap.put(Material.WOODEN_DOOR, 0);		//TODO More research into half-state
+		dataMap.put(Material.IRON_DOOR_BLOCK, 0);	//TODO More research into half-state
+		dataMap.put(Material.STONE_BUTTON, 0);		//TODO More research into pressed state
+		dataMap.put(Material.SIGN_POST, 16);
+		dataMap.put(Material.LADDER, 0);			//TODO More research into missing 0x0 & 0x1
+		dataMap.put(Material.WALL_SIGN, 0);			//TODO More research into missing 0x0 & 0x1
+		dataMap.put(Material.FURNACE, 0);			//TODO More research into missing 0x0 & 0x1
+		dataMap.put(Material.DISPENSER, 0);			//TODO More research into missing 0x0 & 0x1
+		dataMap.put(Material.CHEST, 0);				//TODO More research into missing 0x0 & 0x1
+		dataMap.put(Material.PUMPKIN, 4);
+		dataMap.put(Material.JACK_O_LANTERN, 4);
+		dataMap.put(Material.STONE_PLATE, 0);		//TODO More research into powered states
+		dataMap.put(Material.WOOD_PLATE, 0);		//TODO More research into powered states
+		//TODO Add Coal? Likely not
+		//TODO Tools & Armor? Likely not
+		dataMap.put(Material.STEP, 7);
+		dataMap.put(Material.DOUBLE_STEP, 7);
+		dataMap.put(Material.SNOW, 8);
+		dataMap.put(Material.CAKE_BLOCK, 6);
+		dataMap.put(Material.BED_BLOCK, 0);			//TODO More research into foot/head bit
+		dataMap.put(Material.DIODE_BLOCK_OFF, 0);	//TODO More research into delay/direction
+		dataMap.put(Material.DIODE_BLOCK_ON, 0);	//TODO More research into delay/direction
+		dataMap.put(Material.REDSTONE_WIRE, 0);		//TODO Likely not as it is power level
+		dataMap.put(Material.LONG_GRASS, GrassSpecies.values().length);
+		dataMap.put(Material.TRAP_DOOR, 0);			//TODO More research into open state
+		dataMap.put(Material.PISTON_BASE, 0);		//TODO More research into bit 0x8
+		dataMap.put(Material.PISTON_STICKY_BASE, 0);	//TODO same as above
+		dataMap.put(Material.PISTON_EXTENSION, 0);	//TODO Uhh, no (ignore)
+		dataMap.put(Material.SMOOTH_BRICK, 3);
+		dataMap.put(Material.HUGE_MUSHROOM_1, 11);
+		dataMap.put(Material.HUGE_MUSHROOM_2, 11);
+		dataMap.put(Material.VINE, 16);				//TODO Look into breakdown
+		dataMap.put(Material.FENCE_GATE, 0);		//TODO Look into keeping open state
+		//TODO Potions? Likely not
+		dataMap.put(Material.MONSTER_EGGS, 3);
+		dataMap.put(Material.BREWING_STAND, 0);		//TODO Anything that can be done here?
+		dataMap.put(Material.CAULDRON, 4);
+		dataMap.put(Material.ENDER_PORTAL_FRAME, 4);	//TODO More research into "empty"
+		dataMap.put(Material.EGG, 0);				//TODO More research into spawning
+	}
 
 	@Override
 	public void onDisable() {
@@ -107,30 +122,34 @@ public class YADP extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-        // Register our events
-        getServer().getPluginManager().registerEvents(new ToolListener(), this);
-
-        // Load and/or initialize configuration file
-        this.getConfig();
-        
-        tools.put(Material.STONE_AXE,"dupeTool");
-        //tools.put(Material.SLIME_BALL, "paintTool");
-        //tools.put(Material.DIAMOND_PICKAXE, "jackhamTool");
-        //tools.put(Material.GOLD_AXE, "chainsawTool");
-        //tools.put(Material.GOLD_SPADE, "shovelTool");
-        tools.put(Material.BONE,"scrollTool");
-        //tools.put(Material.GOLD_PICKAXE, "sledgeTool");
-        //tools.put(Material.GLOWSTONE_DUST, "pliersTool");
-        
-        //Print yadp loaded message
-		PluginDescriptionFile pdfFile = this.getDescription();
-		log.info( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );		
+		// Register our events
+		getServer().getPluginManager().registerEvents(new ToolListener(), this);
+		
+		// Load and/or initialize configuration file
+		this.getConfig();
+		
+		tools.put("dupeTool",Material.STONE_AXE);
+		//tools.put("paintTool", Material.SLIME_BALL);
+		//tools.put("jackhamTool", Material.DIAMOND_PICKAXE);
+		//tools.put("chainsawTool", Material.GOLD_AXE);
+		//tools.put("shovelTool", Material.GOLD_SPADE);
+		tools.put("scrollTool",Material.BONE);
+		//tools.put("sledgeTool", Material.GOLD_PICKAXE);
+		//tools.put("pliersTool", Material.GLOWSTONE_DUST);
+		
+		//Print yadp loaded message
+		if(YADP.debug) {
+			PluginDescriptionFile pdfFile = this.getDescription();
+			log.info( "["+pdfFile.getName() + "] version " + pdfFile.getVersion() +
+					" is enabled with debug" );
+		}
 	}
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		Player player = null;
+		//Safety check for determining console status
+		boolean console = true;
 		if (sender instanceof Player) {
-			player = (Player) sender;
+			console = false;
 		}
 		
 		if(cmd.getName().equalsIgnoreCase("yadp")&&(args.length == 1)){
@@ -142,15 +161,23 @@ public class YADP extends JavaPlugin {
 				}
 				return true;
 			} else if (args[0].contentEquals("tools")) {
-				if (player == null) {
+				if (console) {
 					sender.sendMessage("this command can only be run by a player");
 				} else {
-					sender.sendMessage( "In the future this will display what tools you can use");
+					if((sender.hasPermission("yadp.tool.dupe"))&&
+							(YADP.tools.containsKey("dupeTool"))) {
+						sender.sendMessage("Right-click with the "+YADP.tools.get("dupeTool")+
+								" to duplicate the item selected");
+					}
+					if((sender.hasPermission("yadp.tool.scroll"))&&
+							(YADP.tools.containsKey("scrollTool"))) {
+						sender.sendMessage("Click with the "+YADP.tools.get("scrollTool")+
+						" to change a blocks data value");
+					}
 				}
 				return true;
 			}
 		}
-
 		return false; 
 	}
 
